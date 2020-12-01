5a54171731388e036cfd7b81b1f79353d7a475eb
andresjarami <jaramilloandres@hotmail.es>
2020-12-01 16:26:17 +1100

    change dta in input variables (main)

--- Experiments/Experiment1_TimelessDB/mainExp1.py
+++ Experiments/Experiment1_TimelessDB/mainExp1.py
@@ -4,45 +4,29 @@ import sys
 ## Input Variables
 
 
-# featureSet = int(sys.argv[1])
-# startPerson = int(sys.argv[2])
-# endPerson = int(sys.argv[3])
-# place = str(sys.argv[4])
-# typeDatabase = str(sys.argv[5])
-# printR = bool(int(sys.argv[6]))
-# shotStart = int(sys.argv[7])
-# nameFile = place + '_FeatureSet_' + sys.argv[1] + '_startPerson_' + sys.argv[2] + '_endPerson_' + sys.argv[
-#     3] + 'shotStart' + sys.argv[7] + '.csv'
+featureSet = int(sys.argv[1])
+startPerson = int(sys.argv[2])
+endPerson = int(sys.argv[3])
+place = str(sys.argv[4])
+typeDatabase = str(sys.argv[5])
+printR = bool(int(sys.argv[6]))
+shotStart = int(sys.argv[7])
+nameFile = place + '_FeatureSet_' + sys.argv[1] + '_startPerson_' + sys.argv[2] + '_endPerson_' + sys.argv[
+    3] + 'shotStart' + sys.argv[7] + '.csv'
 
-featureSet = 1
-startPerson = 35  # Cote 20-36,EPN 31-60, Nina5 1-10
-endPerson = 35  # Cote 20-36,EPN 31-60, Nina5 1-10
-place = 'resultsExample/exampleE'
-typeDatabase = 'EPN'
-printR = 1
-shotStart = 1
-nameFile = place + '_' + typeDatabase + '_FeatureSet_' + str(featureSet) + '_startPerson_' + str(
-    startPerson) + '_endPerson_' + str(endPerson) + 'shotStart' + str(shotStart) + '.csv'
+# for shotStart in range(1,3):
 
-# # typeDatabaseSet = ['Cote']
-# typeDatabaseSet = ['Cote', 'Nina5']
-# for typeDatabase in typeDatabaseSet:
-#
-#     if typeDatabase == 'Nina5':
-#         startPerson = 1  # Cote 20-36,EPN 31-60, Nina5 1-10
-#         endPerson = 10
-#     elif typeDatabase == 'Cote':
-#         startPerson = 20  # Cote 20-36,EPN 31-60, Nina5 1-10
-#         endPerson = 36
-#
-#     # startPerson = 2 #Cote 20-36,EPN 31-60, Nina5 1-10
-#     # endPerson = 2
-#     featureSet = 1
-#     printR = bool(1)
-#     shotStart = 1
-#     nameFile = typeDatabase + '_FeatureSet_' + str(featureSet) + '_startPerson_' + str(
-#         startPerson) + '_endPerson_' + str(endPerson) + '_shotStart_' + str(shotStart) + '.csv'
-#     # nameFile = None
+# featureSet = 1
+# startPerson = 1  # Cote 20-36,EPN 31-60, Nina5 1-10
+# endPerson = 2  # Cote 20-36,EPN 31-60, Nina5 1-10
+# place = 'resultsExample/exampleN'
+# typeDatabase = 'Nina5'
+# printR = 1
+# shotStart = 2
+# nameFile = place + '_' + typeDatabase + '_FeatureSet_' + str(featureSet) + '_startPerson_' + str(
+#     startPerson) + '_endPerson_' + str(endPerson) + 'shotStart' + str(shotStart) + '.csv'
+
+
 
 # Upload Data
 dataMatrix, _, _, classes, peoplePriorK, _, numberShots, _, allFeatures, _ = functionsExp1.uploadDatabases(
@@ -50,5 +34,4 @@ dataMatrix, _, _, classes, peoplePriorK, _, numberShots, _, allFeatures, _ = fun
 
 # Evaluation
 functionsExp1.evaluation(dataMatrix, classes, peoplePriorK, featureSet, numberShots, nameFile, startPerson,
-                         endPerson,
-                         allFeatures, typeDatabase, printR, shotStart)
+                         endPerson, allFeatures, typeDatabase, printR, shotStart)
